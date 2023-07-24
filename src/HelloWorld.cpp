c007b45837feab9ecc77abd56b4c89fb0a5e43a3
victor-almanzar <105403994+victor-almanzar@users.noreply.github.com>
2023-07-24 08:16:58 +0000

    added some notes to hello world explaining what goes on

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,10 @@
+
+// this is a preprocessor directive. this indicates tha we want to use the 
+// the contents of the std library iostream (Input/Output Stream)
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
