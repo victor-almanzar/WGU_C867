c3f6775a01e631d3291ec776af5c337d6ce19c12
victor-almanzar <105403994+victor-almanzar@users.noreply.github.com>
2023-08-11 15:59:14 +0000

    added .vscode and .devcontainer to gitignore

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,10 +1,7 @@
-
-// this is a preprocessor directive. this indicates tha we want to use the 
-// the contents of the std library iostream (Input/Output Stream)
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-    return 0;
+#include <iostream>                 // this is a preprocessor directive. this indicates tha we want to use 
+                                    // the contents of the standard library iostream (Input/Output Stream)
+int main()                          // every program must have a 'main' fuction
+{                                   // everything within curly braces is part of main function
+    std::cout << "Hello World!\n";  // cout stands for character output
+    return 0;                       // when program finishes running, this send '0' to OS, indicating program worked.q
 }
