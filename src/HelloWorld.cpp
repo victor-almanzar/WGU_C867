8fb69b56611c0bfe2ad96a82b8425c9fd4f66f56
victor-almanzar <105403994+victor-almanzar@users.noreply.github.com>
2023-08-11 16:05:07 +0000

    removed extra comments from helloworld.cpp

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,7 +1,7 @@
-#include <iostream>                 // this is a preprocessor directive. this indicates tha we want to use 
-                                    // the contents of the standard library iostream (Input/Output Stream)
-int main()                          // every program must have a 'main' fuction
-{                                   // everything within curly braces is part of main function
-    std::cout << "Hello World!\n";  // cout stands for character output
-    return 0;                       // when program finishes running, this send '0' to OS, indicating program worked.q
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
 }
