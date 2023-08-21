53d0b692f77ca2ab2e63bc4fe9166c959dbc90f6
victor-almanzar <105403994+victor-almanzar@users.noreply.github.com>
2023-08-21 23:21:57 +0000

    testing cout

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{   
+    cout << "¿cuanto ganas por hora? ";
+    int wage;
+    cin >> wage;
+
+    cout << "¿cuanto ganas en propinas al mes? ";
+    int tips;
+    cin >> tips;
+
+    cout << "\nSuponiendo que trabaja 40 horas a la semana durante todo el año, su salario anual es...1 \n\n";
+    cout << wage * 40 * 52 + (tips * 12);
+    cout << "\n\n";
+    cout << endl;
+
     return 0;
 }
